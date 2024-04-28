37e2a3b14354eaa25fb6ba4a34291bfbfa288b77
Saeedinum <mo7amed0ptr@gmail.com>
2024-04-28 15:31:53 +0300

    non-serializiable

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 
-import {persistStore, persistReducer} from "redux-persist";
+import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import Categories from "./Categories/categoriesSlice";
@@ -16,6 +16,12 @@ const store = configureStore({
 		},
 		combineReducers({categories: Categories, products: Products, cart: cart}),
 	),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 let persistor = persistStore(store);
