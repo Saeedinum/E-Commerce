33b8369313011c6028c79d6ed52bc0e4e7a51bd4
Saeedinum <mo7amed0ptr@gmail.com>
2024-05-04 11:52:18 +0300

    ContentLoader

--- src/components/feedback/Loading/Loading.tsx
+++ src/components/feedback/Loading/Loading.tsx
@@ -1,15 +1,24 @@
+import ContentLoader from "react-content-loader";
+
 type Props = {
 	loading: "idle" | "pending" | "succeeded" | "failed";
 	error: string | null;
 	children: React.ReactNode;
 };
 
+const MyLoader = () => (
+	<ContentLoader speed={2} width={535} height={258} viewBox='0 0 535 258' backgroundColor='#efebeb' foregroundColor='#d1d4ff'>
+		<circle cx='81' cy='81' r='76' />
+		<rect x='27' y='174' rx='0' ry='0' width='116' height='13' />
+	</ContentLoader>
+);
+
 const Loading = ({loading, error, children}: Props) => {
 	if (loading === "succeeded") {
 		return <>{children}</>;
 	}
 	if (loading === "pending") {
-		return <div>Loading</div>;
+		return <MyLoader />;
 	}
 	if (loading === "failed") {
 		return <div>Error: {error}</div>;
